a3f010a68ba4115c23e437db369ade1ac076c54e
bsm52 <bsm52@zips.uakron.edu>
2019-05-01 19:46:52 -0400

    2048 game with test

---
+++ build/CMakeFiles/2048_t.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/2048_t.dir/2048_t.cpp.o"
+  "2048_t.pdb"
+  "2048_t"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/2048_t.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
